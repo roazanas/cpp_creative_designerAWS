1123e4ebd463085ecf2e728fee127f632f151c35
Nikita <anonumlol@gmail.com>
2024-05-06 13:56:36 +0500

    First prototype (layout) for designerAWS

---
+++ build/Desktop_Qt_6_7_0_MinGW_64_bit-Debug/ui_mainwindow.h
@@ -0,0 +1,172 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 6.7.0
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtGui/QAction>
+#include <QtGui/QIcon>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenu>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPlainTextEdit>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QWidget *centralwidget;
+    QHBoxLayout *horizontalLayout;
+    QPlainTextEdit *infoPlain;
+    QLabel *image;
+    QVBoxLayout *verticalLayout;
+    QPushButton *pushButton;
+    QPushButton *pushButton_2;
+    QPushButton *pushButton_3;
+    QPushButton *pushButton_4;
+    QPushButton *pushButton_5;
+    QMenuBar *menuBar;
+    QMenu *menuSettings;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName("MainWindow");
+        MainWindow->resize(816, 385);
+        MainWindow->setMaximumSize(QSize(16777215, 16777215));
+        QIcon icon;
+        QString iconThemeName = QString::fromUtf8("applications-graphics");
+        if (QIcon::hasThemeIcon(iconThemeName)) {
+            icon = QIcon::fromTheme(iconThemeName);
+        } else {
+            icon.addFile(QString::fromUtf8("res/icon.svg"), QSize(), QIcon::Normal, QIcon::Off);
+        }
+        MainWindow->setWindowIcon(icon);
+        centralwidget = new QWidget(MainWindow);
+        centralwidget->setObjectName("centralwidget");
+        horizontalLayout = new QHBoxLayout(centralwidget);
+        horizontalLayout->setObjectName("horizontalLayout");
+        infoPlain = new QPlainTextEdit(centralwidget);
+        infoPlain->setObjectName("infoPlain");
+        QSizePolicy sizePolicy(QSizePolicy::Policy::Fixed, QSizePolicy::Policy::Minimum);
+        sizePolicy.setHorizontalStretch(0);
+        sizePolicy.setVerticalStretch(0);
+        sizePolicy.setHeightForWidth(infoPlain->sizePolicy().hasHeightForWidth());
+        infoPlain->setSizePolicy(sizePolicy);
+        infoPlain->setMinimumSize(QSize(0, 0));
+        infoPlain->setReadOnly(true);
+
+        horizontalLayout->addWidget(infoPlain);
+
+        image = new QLabel(centralwidget);
+        image->setObjectName("image");
+        QSizePolicy sizePolicy1(QSizePolicy::Policy::Ignored, QSizePolicy::Policy::Ignored);
+        sizePolicy1.setHorizontalStretch(0);
+        sizePolicy1.setVerticalStretch(0);
+        sizePolicy1.setHeightForWidth(image->sizePolicy().hasHeightForWidth());
+        image->setSizePolicy(sizePolicy1);
+        image->setPixmap(QPixmap(QString::fromUtf8("res/stub.jpg")));
+        image->setScaledContents(true);
+
+        horizontalLayout->addWidget(image);
+
+        verticalLayout = new QVBoxLayout();
+        verticalLayout->setObjectName("verticalLayout");
+        verticalLayout->setContentsMargins(0, -1, -1, -1);
+        pushButton = new QPushButton(centralwidget);
+        pushButton->setObjectName("pushButton");
+        QSizePolicy sizePolicy2(QSizePolicy::Policy::Fixed, QSizePolicy::Policy::Fixed);
+        sizePolicy2.setHorizontalStretch(0);
+        sizePolicy2.setVerticalStretch(0);
+        sizePolicy2.setHeightForWidth(pushButton->sizePolicy().hasHeightForWidth());
+        pushButton->setSizePolicy(sizePolicy2);
+
+        verticalLayout->addWidget(pushButton);
+
+        pushButton_2 = new QPushButton(centralwidget);
+        pushButton_2->setObjectName("pushButton_2");
+        sizePolicy2.setHeightForWidth(pushButton_2->sizePolicy().hasHeightForWidth());
+        pushButton_2->setSizePolicy(sizePolicy2);
+
+        verticalLayout->addWidget(pushButton_2);
+
+        pushButton_3 = new QPushButton(centralwidget);
+        pushButton_3->setObjectName("pushButton_3");
+        sizePolicy2.setHeightForWidth(pushButton_3->sizePolicy().hasHeightForWidth());
+        pushButton_3->setSizePolicy(sizePolicy2);
+
+        verticalLayout->addWidget(pushButton_3);
+
+        pushButton_4 = new QPushButton(centralwidget);
+        pushButton_4->setObjectName("pushButton_4");
+        sizePolicy2.setHeightForWidth(pushButton_4->sizePolicy().hasHeightForWidth());
+        pushButton_4->setSizePolicy(sizePolicy2);
+
+        verticalLayout->addWidget(pushButton_4);
+
+        pushButton_5 = new QPushButton(centralwidget);
+        pushButton_5->setObjectName("pushButton_5");
+        sizePolicy2.setHeightForWidth(pushButton_5->sizePolicy().hasHeightForWidth());
+        pushButton_5->setSizePolicy(sizePolicy2);
+
+        verticalLayout->addWidget(pushButton_5);
+
+
+        horizontalLayout->addLayout(verticalLayout);
+
+        MainWindow->setCentralWidget(centralwidget);
+        menuBar = new QMenuBar(MainWindow);
+        menuBar->setObjectName("menuBar");
+        menuBar->setGeometry(QRect(0, 0, 816, 25));
+        menuSettings = new QMenu(menuBar);
+        menuSettings->setObjectName("menuSettings");
+        MainWindow->setMenuBar(menuBar);
+
+        menuBar->addAction(menuSettings->menuAction());
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "Designer tool", nullptr));
+        infoPlain->setPlainText(QCoreApplication::translate("MainWindow", "Some useful info\n"
+"Another some useful info\n"
+"INFO | INFO | INFO | INFO\n"
+"\n"
+"\n"
+"\n"
+"Useless info", nullptr));
+        image->setText(QString());
+        pushButton->setText(QCoreApplication::translate("MainWindow", "Tool1", nullptr));
+        pushButton_2->setText(QCoreApplication::translate("MainWindow", "Tool2", nullptr));
+        pushButton_3->setText(QCoreApplication::translate("MainWindow", "Tool3", nullptr));
+        pushButton_4->setText(QCoreApplication::translate("MainWindow", "Tool4", nullptr));
+        pushButton_5->setText(QCoreApplication::translate("MainWindow", "Tool5", nullptr));
+        menuSettings->setTitle(QCoreApplication::translate("MainWindow", "settings", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
